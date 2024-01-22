8226920dc91ff8931dbae6335b6053e2b5c5ea7c
Asif Uddin <uasif13@gmail.com>
2024-01-22 10:01:08 -0500

    added transporter and changed http method

--- node.js
+++ node.js
@@ -2,9 +2,17 @@ const app = express()
 const port = 3000
 const nodemailer = require("nodemailer")
 
-// const transporter = 
+const transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "uasif13@gmail.com",
+        pass: "asdfghjkl;'1234567890"
+    }
+})
 
-app.get("/formData", get, (name,email,body) => {
+app.get("/formData", post, (name,email,body) => {
     const name = name
     const email = email
     const body = body
